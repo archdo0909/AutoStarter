de55f08c87207f8843ec682c82dcf9b5186e1970
Doyeon Kim <archdo0909@gmail.com>
2020-07-21 21:48:47 +0900

    Update searching file system tree and execute shell script

---
+++ 01-config/01-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "WowWowWowWow"
+
+#pushd "/home/modi"
+
+#jupyter notebook --generate-config
+
+#popd
--- auto_config.sh
+++ auto_config.sh
@@ -1,18 +1,42 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# Excute as sudoer
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
 
 # Set current folder
 export CONFIG_HOME_DIR=${PWD}
 echo "${CONFIG_HOME_DIR}"
+
+SET=$(seq -f "%02g" 0 10)
 # Extract target folder
 
 for file in ${CONFIG_HOME_DIR}/*; do 
     if [ -d "${file}" ]; then
 
-        echo "found"
         pushd "${file}" > /dev/null
 
         if [ -f SKIP ]; then
-            echo "SKIP"
+            popd
+        
+        elif [ ! -f SKIP ]; then
+            echo "there is no SKIP"
+            #for i in $SET; do
+            for i in $SET; do
+                if [ -f "${i}-setup.sh" ]; then
+                    echo "start setup.sh"
+                    ./${i}-setup.sh
+                fi
+            done
+            touch SKIP
             popd
         fi
     fi
