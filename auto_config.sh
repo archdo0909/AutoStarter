b1daaa8604bacd0318c70b62f2d368ffec2593f7
Doyeon Kim <archdo0909@gmail.com>
2020-07-21 15:06:47 +0900

    Add target directory search

--- auto_config.sh
+++ auto_config.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 # Set current folder
-export HOME_DIR
-
-${HOME_DIR} = 
-
+export CONFIG_HOME_DIR=${PWD}
+echo "${CONFIG_HOME_DIR}"
 # Extract target folder
 
+for file in ${CONFIG_HOME_DIR}/*; do 
+    if [ -d "${file}" ]; then
+
+        echo "found"
+        pushd "${file}" > /dev/null
+
+        if [ -f SKIP ]; then
+            echo "SKIP"
+            popd
+        fi
+    fi
+done
 
 # Check installed flag
 
