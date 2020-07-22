ff605a39b82227a1c59a4cbb8115e0a2ca9b4dfa
Doyeon Kim <archdo0909@gmail.com>
2020-07-22 12:27:31 +0900

    Apply diff patch jupyter config

--- 01-config/01-setup.sh
+++ 01-config/01-setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# generate jupyter notebook config python file
+
 pushd "/home/modi" && jupyter notebook --generate-config
 
 if [ ! -d .jupyter ]; then
 	mv "/root/.jupyter" "/home/modi" 
 fi
+
+popd
+# find diff file
+CURRENT_PATH="${PWD}/00-patches"
+pushd ${CURRENT_PATH}
+if [ -f jupyter_config.diff ]; then
+    patch jupyter_config.diff
+fi
+
 popd
