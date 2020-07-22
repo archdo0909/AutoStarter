595bfedec4ad76d9385528007e1dbad2eb8718e8
Doyeon <archdo0909@gmail.com>
2020-07-22 07:59:46 +0100

    Apply custom jupyter notebook

--- 01-config/01-setup.sh
+++ 01-config/01-setup.sh
@@ -5,7 +5,8 @@
 pushd "/home/modi" && jupyter notebook --generate-config
 
 if [ ! -d .jupyter ]; then
-	mv "/root/.jupyter" "/home/modi" 
+	mv "/root/.jupyter" "/home/modi"
+        sudo chmod -R +rx /home/modi/.jupyter 	
 fi
 
 popd
@@ -13,7 +14,11 @@ popd
 CURRENT_PATH="${PWD}/00-patches"
 pushd ${CURRENT_PATH}
 if [ -f jupyter_config.diff ]; then
-    patch jupyter_config.diff
+    patch -d/ -p0 < jupyter_config.diff
 fi
 
+cp -rf /usr/src/wificonnection/wificonnection /home/modi/.local/lib/python3.7/site-packages
+jupyter serverextension enable --py wificonnection --user
+jupyter nbextension install --py wificonnection --user
+jupyter nbextension enable --py wificonnection --user
 popd
