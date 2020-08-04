1f72dc0b2374bab93cf38a97805873522933d845
Doyeon Kim <archdo0909@gmail.com>
2020-08-04 11:35:02 +0900

    Update code reset

--- code_reset.sh
+++ code_reset.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo rm -rf ~/workspace/ai-contents-gyro-car
-sudo cp -r /usr/src/ai-contents-gyro-car ~/workspace
+sudo rm -rf ~/workspace/ai-contents-gyro-car/model
+sudo rm -rf ~/workspace/ai-contents-gyro-car/src
+sudo cp -r /usr/src/ai-contents-gyro-car/model ~/workspace/ai-contents-gyro-car
+sudo cp -r /usr/src/ai-contents-gyro-car/src ~/workspace/ai-contents-gyro-car
 sudo chmod -R 777 ~/workspace/ai-contents-gyro-car
